8144687e52fa1a27073ce71f53e2525c7b4d33ca
Anica <3c.innertherapy@gmail.com>
2026-03-29 14:26:41 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -40,7 +40,7 @@
     window.sb ||
     window.supabase.createClient(
       window.APP_CONFIG.SUPABASE_URL,
-      window.APP_CONFIG.SUPABASE_ANON_KEY
+      window.APP_CONFIG.SUPABASE_KEY
     );
   window.sb = sb;
 
